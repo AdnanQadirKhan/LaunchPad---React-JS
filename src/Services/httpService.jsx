069821fac1d960ef4bfc0ac55590447351027d3e
M-Adnan <m.adnan6425@gmail.com>
2023-04-18 01:50:31 +0500

    kyc , audit add api

---
+++ src/Services/httpService.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { enqueueSnackbar } from "notistack";
+axios.defaults.baseURL = "http://localhost:3004/api/";
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    enqueueSnackbar("An un expected error occured.", { variant: "error" });
+  }
+
+  return Promise.reject(error);
+});
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
